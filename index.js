f8713f69bca8efe0593d256310bb9c027ba3a69d
abdulmoiez876 <74975190+abdulmoiez876@users.noreply.github.com>
2022-01-18 17:01:37 +0500

    Added initials for drumKit simulator

---
+++ index.js
@@ -0,0 +1,65 @@
+// Functions
+function onClick(soundKey) {
+
+    switch (soundKey) {
+        case 'w':
+            var crash = new Audio('sounds/crash.mp3');
+            crash.play();
+            break;
+        case 'a':
+            var kickBass = new Audio('sounds/kick-bass.mp3');
+            kickBass.play();
+            break;
+        case 's':
+            var snare = new Audio('sounds/snare.mp3');
+            snare.play();
+            break;
+        case 'd':
+            var tom1 = new Audio('sounds/tom-1.mp3');
+            tom1.play();
+            break;
+        case 'j':
+            var tom2 = new Audio('sounds/tom-2.mp3');
+            tom2.play();
+            break;
+        case 'k':
+            var tom3 = new Audio('sounds/tom-3.mp3');
+            tom3.play();
+            break;
+        case 'l':
+            var tom4 = new Audio('sounds/tom-4.mp3');
+            tom4.play();
+            break;
+    }
+}
+
+// for animation
+function buttonAnimation(buttonPressed) {
+    document.querySelector('.' + buttonPressed).classList.add('pressed');
+}
+
+
+// *************************************************************************************
+
+
+// Event listeners
+for (var i = 0; i < document.querySelectorAll('.drum').length; i++) {
+    document.querySelectorAll('.drum')[i].addEventListener('click', function() {
+        var soundKey = this.innerHTML;
+        onClick(soundKey);
+        buttonAnimation(soundKey);
+        setTimeout(function() {
+            document.querySelector('.' + soundKey).classList.remove('pressed');
+        }, 100);
+    });
+}
+
+// for keyboard presses
+document.addEventListener('keydown', function(event) {
+    var soundKey = event.key;
+    onClick(soundKey);
+    buttonAnimation(soundKey);
+    setTimeout(function() {
+        document.querySelector('.' + soundKey).classList.remove('pressed');
+    }, 100);
+});
